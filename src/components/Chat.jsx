8df3168fd016fbd8f48c4e0c626fcce94252db5f
Ricci <riccidandrada@gmail.com>
2022-01-10 22:05:06 -0500

    getting data from firebase

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,25 @@
 import SignOut from "./SignOut";
-
+import {useState,useEffect} from 'react'
+import { db } from "../firebase";
 function Chat() {
+    const [message,setMessage]=useState([])
+
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit('50').onSnapshot( (snapshot) =>(
+             setMessage(snapshot.docs.map( doc => doc.data())) 
+             ))
+        
+    }, [])
     return (
         <div>
             Chat
             <SignOut/>
+            {message.map(({id,text,photoURL})=>(
+                 <div key={id} >
+                    <img  src={photoURL} alt="" ></img>
+                    <p>{text}</p>
+                </div>
+            ))}
 
         </div>
     )
